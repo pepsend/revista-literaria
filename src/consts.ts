e83e360477ecdba79ecd4a4d1e418d3b3bf87487
Víctor Fleitas <victorfleitas@MacBook-Air-de-Victor.local>
2026-08-05 13:07:07 -0400

    Cambiar titulo a Abulia Terminal

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Abulia Terminal';
+export const SITE_TITLE = 'abulia terminal';
 export const SITE_DESCRIPTION = 'Revista literaria';
